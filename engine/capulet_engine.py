88c021da2e73a70cb043a030339c4e8eeb683756
slamchillz <mendyslam@gmail.com>
2023-06-02 15:46:27 +0100

    made servicing criterias private attributes

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -2,9 +2,12 @@ from engine.engine import Engine
 
 
 class CapuletEngine(Engine):
+
+    __milage_limit = 30000
+
     def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
     def needs_service(self):
-        return self.current_mileage - self.last_service_mileage > 30000
+        return self.current_mileage - self.last_service_mileage > self.__milage_limit
