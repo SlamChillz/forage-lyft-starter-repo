90cea730958d2f3a7b493b31972b5f0211dc0a67
slamchillz <mendyslam@gmail.com>
2023-06-02 12:37:13 +0100

    created date instance a better way

--- tests/test_battery/test_spindler.py
+++ tests/test_battery/test_spindler.py
@@ -1,20 +1,20 @@
 import unittest
-from datetime import datetime
+from datetime import date
 
 from battery.spindler_battery import SpindlerBattery
 
 
 class TestSpindlerBattery(unittest.TestCase):
     def test_spindler_battery_should_be_serviced(self):
-        current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 3)
+        current_date = date.fromisoformat("2023-06-02")
+        last_service_date = date.fromisoformat("2021-05-01")
 
         battery = SpindlerBattery(last_service_date, current_date)
         self.assertTrue(battery.needs_service())
 
     def test_spindler_battery_should_not_be_serviced(self):
-        current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 1)
+        current_date = date.fromisoformat("2023-06-02")
+        last_service_date = date.fromisoformat("2022-05-01")
 
         battery = SpindlerBattery(last_service_date, current_date)
         self.assertFalse(battery.needs_service())
